c6c16f70c884a89140e7d5adec8d82bb5127e83d
michelr <michel.rousset@free.fr>
2022-12-16 15:18:30 +0100

    v0.1.2

--- README.md
+++ README.md
@@ -1,8 +1,19 @@
 # mts-npm-series
 
+
 A series of mini projects (named mts-npm-X) to learn npm packages building.
 
 mts stands for something like Microu's typescript journey.
 
 * [mts-npm-a](./mts-npm-a/README.md): a basic javascript package
 * mts-npm-b: a basic typescript package
+
+## Release history
+
+### v0.1.0 20221216
+
+First release, implementing `greet` function.
+
+### v0.1.1 20221216
+
+Replace `greet` function y randomNumber function.
--- mts-npm-a/src/index.js
+++ mts-npm-a/src/index.js
@@ -1,6 +1,20 @@
-export function greet(name) {
-    if (name == undefined) {
-        name = "World"
+export function randomNumber(arg0, arg1, arg2) {
+  if (arg0 == undefined) {
+    return Math.random();
+  }
+  if (arg0 != undefined && arg1 == undefined) {
+    return Math.floor(Math.random() * Math.round(arg0));
+  }
+
+  if (arg0 != undefined && arg1 != undefined && arg2 == undefined) {
+    return arg0 + Math.random() * (arg1 - arg0);
+  }
+
+  if (arg0 != undefined && arg1 != undefined && arg2 != undefined) {
+    let nsteps = Math.ceil((arg1 - arg0) / arg2);
+    if (arg0 + nsteps * arg2 >= arg1 ) {
+      nsteps -= 1;
     }
-    return `Hello ${name}`
+    return arg0 + Math.floor(Math.random() * (nsteps +1)) * arg2;
+  }
 }
